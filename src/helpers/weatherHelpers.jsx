f87e851914bb0d023fab0442d518ad3680800440
Dave <dave.w.mckinnon@gmail.com>
2022-10-08 13:04:40 -0700

    fix time locale issue

--- src/helpers/weatherHelpers.jsx
+++ src/helpers/weatherHelpers.jsx
@@ -1,8 +1,16 @@
 const UTC = "2022-10-05T02:00Z";
 
 const convertUTC = (utcTime) => {
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  };
   const localDate = new Date(utcTime)
-  return(localDate.toLocaleString());
+  return(localDate.toLocaleString('en-US', options));
 };
 
 convertUTC(UTC);
