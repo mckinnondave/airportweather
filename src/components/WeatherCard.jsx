3bb8860a1b15c4ddc4cc24cd408eeee6abae06ae
Dave <dave.w.mckinnon@gmail.com>
2022-10-12 19:47:44 -0700

    add comments to weathercard and fix height but for when no cloud data comes back

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -4,6 +4,7 @@ import { convertUTC, windDirection } from "../helpers/weatherHelpers";
 const WeatherCard = ({ metarData, tafData }) => {
   return (
     <div className="weatherCard">
+      {/* Upper portion of weather card (Station name and elevation)*/}
       {metarData.map((airport, index) => (
         <div className="airport__container" key={airport.station.name}>
           <div className="airport__banner">
@@ -16,12 +17,14 @@ const WeatherCard = ({ metarData, tafData }) => {
             </div>
           </div>
           <div className="metarAndTaf">
+            {/* METAR portion of weather card*/}
             <div className="metar">
               <div className="metarAndTaf__banner">Recent Weather</div>
               <div className="metar__reportTime">
                 Weather reported on {convertUTC(airport.observed)}
               </div>
               <div className="metar__dataContainer">
+                {/* Quadrant of data for temperature, wind, visibiltiy, and humidity */}
                 <div className="metar__left">
                   <div className="metar__left__top">
                     <div className="metar__left__temp">
@@ -34,16 +37,15 @@ const WeatherCard = ({ metarData, tafData }) => {
                       <div className="metar__left__banner">Wind</div>
                       {airport.wind ? (
                         <div className="metar__left__wind__data">
-                        Coming from: <br />
-                        <br />
-                        {windDirection(airport.wind.degrees)} (
-                        {airport.wind.degrees}&#176;) at{" "}
-                        {airport.wind.speed_kph}kph
-                      </div>
+                          Coming from: <br />
+                          <br />
+                          {windDirection(airport.wind.degrees)} (
+                          {airport.wind.degrees}&#176;) at{" "}
+                          {airport.wind.speed_kph}kph
+                        </div>
                       ) : (
                         <div className="metar__left__wind__data">N/A</div>
                       )}
-                      
                     </div>
                   </div>
                   <div className="metar__left__bottom">
@@ -62,8 +64,8 @@ const WeatherCard = ({ metarData, tafData }) => {
                   </div>
                 </div>
                 <div className="metar__right">
+                  {/* display area on right side of METAR section for cloud info*/}
                   <div className="metar__right__banner">Clouds</div>
-
                   <div className="metar__clouds">
                     <div className="metar__clouds__top">
                       <div className="metar__clouds__topIndex">Layer</div>
@@ -80,15 +82,21 @@ const WeatherCard = ({ metarData, tafData }) => {
                         <div className="metar__clouds__bottomType">
                           {cloud.text}
                         </div>
-                        <div className="metar__clouds__bottomHeight">
-                          {cloud.feet}'
-                        </div>
+                        {cloud.feet ? (
+                          <div className="metar__clouds__bottomHeight">
+                            {cloud.feet}'
+                          </div>
+                        ) : (
+                          <div className="metar__clouds__bottomHeight">N/A</div>
+                        )}
                       </div>
                     ))}
                   </div>
                 </div>
               </div>
             </div>
+
+            {/* TAF portion of weather card */}
             <div className="taf">
               <div className="metarAndTaf__banner">Forecast</div>
               <div className="metar__reportTime">
