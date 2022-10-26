f31e2560e5e5dfb1819685a21b5bec4017f1ee11
Dave <dave.w.mckinnon@gmail.com>
2022-10-26 14:07:05 -0700

    add accordion for TAF

--- src/components/Taf.jsx
+++ src/components/Taf.jsx
@@ -1,5 +1,11 @@
 import { convertUTC } from "../helpers/weatherHelpers";
-import './Taf.scss'
+import "./Taf.scss";
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const Taf = ({ tafData, index }) => {
   return (
@@ -8,9 +14,46 @@ const Taf = ({ tafData, index }) => {
       <div className="metar__reportTime">
         Forecast issued on {convertUTC(tafData[index].timestamp.issued)}
       </div>
+      {console.log("TAF DATA", tafData[index].forecast)}
+
       <div className="taf__container">
-        From {convertUTC(tafData[index].forecast[0].timestamp.from)} until{" "}
-        {convertUTC(tafData[index].forecast[0].timestamp.to)}
+        {tafData[index].forecast.map((forecast) => (
+          <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Accordion 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        ))}
+        
+        {/* <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography>Accordion 2</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion> */}
+        {/* From {convertUTC(tafData[index].forecast[0].timestamp.from)} until{" "}
+        {convertUTC(tafData[index].forecast[0].timestamp.to)} */}
       </div>
     </div>
   );
