a998120f1f3e463d46745d987cdfce32b610a4c0
Dave <dave.w.mckinnon@gmail.com>
2022-11-01 17:38:11 -0700

    remove text over bubbles in bubble chart and remove x-axis label

--- src/components/TempChart.jsx
+++ src/components/TempChart.jsx
@@ -7,14 +7,16 @@ const TempChart = ({clouds}) => {
   const options = {
     animationEnabled: true,
     exportEnabled: true,
-    theme: "dark1", // "light1", "light2", "dark1", "dark2"
+    theme: "dark1",
     title: {
       text: "Clouds",
       fontSize: 26,
     },
     axisX: {
       title: "Ground",
-      logarithmic: false,
+      labelFormatter: function(){
+        return " "
+      }
     },
     axisY: {
       title: "Altitude (Above Ground Level)",
@@ -22,26 +24,17 @@ const TempChart = ({clouds}) => {
     data: [
       {
         type: "bubble",
-        indexLabel: "{label}",
-        
+                
         toolTipContent:
           "<b>{label}</b><br>Distance From Sun: {x}mn miles<br>Avg. Surface Temp: {y} Kelvin<br>Diameter: {z} miles",
-        dataPoints: [
-          // { label: "Mercury", x: 0, y: 452, z: 3031 },
-          // { label: "Venus", x: 0, y: 726, z: 7521 },
-          // { label: "Earth", x: 0, y: 285, z: 7926 },
-          // { label: "Mars", x: 0, y: 230, z: 4222 },
-          // { label: "Jupiter", x: 0, y: 120, z: 88729 },
-          // { label: "Saturn", x: 0, y: 88, z: 74600 },
-          // { label: "Uranus", x: 0, y: 59, z: 32600 },
-          // { label: "Neptune", x: 0, y: 48, z: 30200 },
-        ],
+        dataPoints: [],
       },
     ],
   };
-
+  
+  // Create datapoints with cloud info
   for (const cloud of clouds) {
-    (options.data[0].dataPoints).push({label: `${cloud.text} @ ${cloud.feet}`, x: 0, y: cloud.feet, z: cloud.feet+5000, indexLabelTextAlign: "right"})
+    (options.data[0].dataPoints).push({label: cloud.text, x: 0, y: cloud.feet, z: cloud.feet+5000, indexLabelTextAlign: "left"})
   }
 
   return (
