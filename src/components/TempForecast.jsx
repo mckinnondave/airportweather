cf28ce15a7f3672a753d6a3307aea1d83b0078a0
Dave <dave.w.mckinnon@gmail.com>
2022-11-04 11:58:42 -0700

    add options for temp chart

--- src/components/TempForecast.jsx
+++ src/components/TempForecast.jsx
@@ -1,33 +1,33 @@
 import CanvasJSReact from "../canvas/canvasjs.react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { getLocaleDate } from "../helpers/weatherHelpers";
+import { getLocaleDate, timeConvertUTC } from "../helpers/weatherHelpers";
 
-const TempForecast = ({latitude, longitude}) => {
-  const [tempData, setTempData] = useState([])
-  const [tempTimeData, setTempTimeData] = useState([])
+const TempForecast = ({ latitude, longitude }) => {
+  const [tempData, setTempData] = useState([]);
+  const [tempTimeData, setTempTimeData] = useState([]);
   const todaysDate = getLocaleDate();
-  console.log("TEMP", tempData)
-  console.log("TEMPTIME", tempTimeData)
+  console.log("TEMP", tempData);
+  console.log("TEMPTIME", tempTimeData);
 
   let CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
   useEffect(() => {
     const callForTempData = (lat, lon) => {
-      axios.get(
-        `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&timezone=auto&start_date=${todaysDate}&end_date=${todaysDate}`
-      )
-        .then(res => {
-          setTempData(res.data.hourly.temperature_2m)
-          setTempTimeData(res.data.hourly.time)
+      axios
+        .get(
+          `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&timezone=auto&start_date=${todaysDate}&end_date=${todaysDate}`
+        )
+        .then((res) => {
+          setTempData(res.data.hourly.temperature_2m);
+          setTempTimeData(res.data.hourly.time);
         })
-        
         .catch((err) => {
           console.log("ERROR", err);
         });
     };
-    callForTempData(latitude, longitude)
-  }, [latitude, longitude, todaysDate])
+    callForTempData(latitude, longitude);
+  }, [latitude, longitude, todaysDate]);
 
   const options = {
     animationEnabled: true,
@@ -42,46 +42,24 @@ const TempForecast = ({latitude, longitude}) => {
     },
     axisX: {
       title: "Day of Week",
-      interval: 1,
+      valueFormatString: "HH:mm"
     },
     data: [
       {
         type: "line",
         toolTipContent: "Week {x}: {y}%",
-        dataPoints: [
-          { x: 1, y: 64 },
-          { x: 2, y: 61 },
-          { x: 3, y: 64 },
-          { x: 4, y: 62 },
-          { x: 5, y: 64 },
-          { x: 6, y: 60 },
-          { x: 7, y: 58 },
-          { x: 8, y: 59 },
-          { x: 9, y: 53 },
-          { x: 10, y: 54 },
-          { x: 11, y: 61 },
-          { x: 12, y: 60 },
-          { x: 13, y: 55 },
-          { x: 14, y: 60 },
-          { x: 15, y: 56 },
-          { x: 16, y: 60 },
-          { x: 17, y: 59.5 },
-          { x: 18, y: 63 },
-          { x: 19, y: 58 },
-          { x: 20, y: 54 },
-          { x: 21, y: 59 },
-          { x: 22, y: 64 },
-          { x: 23, y: 59 },
-        ],
+        xValueType:"dateTime",
+        dataPoints: [],
       },
     ],
   };
+  
+  for (let i = 0; i < tempData.length; i++) {
+    options.data[0].dataPoints.push({ x: tempTimeData[i], y: tempData[i] });
+  }
+  console.log(options.data[0].dataPoints);
 
-  return (
-      <CanvasJSChart
-        options={options}
-      />
-  );
+  return <CanvasJSChart options={options} />;
 };
 
 export default TempForecast;
